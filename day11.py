65d1dcba343b721b5f7f42529f03a568c875d2c7
Simonolsson98 <Simol@student.chalmers.se>
2020-12-28 23:05:22 +0100

    day11 solution final

--- day11.py
+++ day11.py
@@ -8,80 +8,73 @@ def main():
         lines.append(list(i[:len(i)-1]))
         i = input.readline()
 
+    #deepcopy needed here, since lines is a list of lists
     new_lines = copy.deepcopy(lines) #the list used to check if a round changed our starting list
-    stopped_changing_or_not = 0
+    
     while True:
         for i in range(len(lines)):
             for j in range(len(lines[i])):
                 seat = lines[i][j]
-                occupied_seats = get_occupied_seats(lines, i, j, [], stopped_changing_or_not)
+                neighbouring_seats = get_occupied_seats(lines, i, j, []) #retrieve the neighbouring seats
 
-                if seat == 'L' and '#' not in occupied_seats:
-                    stopped_changing_or_not = 0
-                    new_lines[i][j] = '#'
-                elif seat == '#' and occupied_seats.count('#') >= 4:
-                    stopped_changing_or_not = 0
-                    new_lines[i][j] = 'L'
-                else: #seat = '.' 
-                    stopped_changing_or_not += 1
-                    continue
-                occupied_seats.clear()
-        if lines == new_lines: #no change from 1 round, so we should stop
+                if seat == 'L' and '#' not in neighbouring_seats:
+                    new_lines[i][j] = '#' #seat should be occupied
+                elif seat == '#' and neighbouring_seats.count('#') >= 4:
+                    new_lines[i][j] = 'L' #seat gets freed
+                else: #seat = '.' or both of the above requirements were not met
+                    pass
+
+        if lines == new_lines: #no change from the round that just executed, so we should stop
             result = 0
             for k in range(len(new_lines)):
                 result += new_lines[k].count('#') #count occurrences of # in every line
             return result
         else: #prepare for next round
-            lines = copy.deepcopy(new_lines)
+            lines = copy.deepcopy(new_lines)#deepcopy needed here, since lines is a list of lists
 
 
-def get_occupied_seats(lines, rowindex, colindex, occupied_seats, stop):
-    #print(stop)
-    if(colindex - 1 < 0):
+def get_occupied_seats(lines, row_index, col_index, neighbouring_seats): #function that returns a list of all occupied neighbouring seats
+    #checks for all types of indices, also makes sure that for example the first element only has 3 neighbours, since L # gives ['#', '.', '#'] for L
+    #                                                                                                                 . #   
+    
+    if(col_index - 1 < 0):
         pass
     else:
-        occupied_seats.append(lines[rowindex][colindex-1])
-
-    if colindex + 1 > len(lines[rowindex]) - 1:
+        neighbouring_seats.append(lines[row_index][col_index-1])
+    if col_index + 1 > len(lines[row_index]) - 1:
         pass
     else:
-        occupied_seats.append(lines[rowindex][colindex+1])
-
-    if(colindex - 1 < 0 or rowindex + 1 > len(lines) - 1):
+        neighbouring_seats.append(lines[row_index][col_index+1])
+    if(col_index - 1 < 0 or row_index + 1 > len(lines) - 1):
         pass
     else:
-        occupied_seats.append(lines[rowindex+1][colindex-1])
-            
-    if(rowindex + 1 > len(lines) - 1 or colindex + 1 > len(lines[rowindex + 1]) - 1):
+        neighbouring_seats.append(lines[row_index+1][col_index-1])
+    if(row_index + 1 > len(lines) - 1 or col_index + 1 > len(lines[row_index + 1]) - 1):
         pass
     else:
-        occupied_seats.append(lines[rowindex+1][colindex+1])
+        neighbouring_seats.append(lines[row_index+1][col_index+1])
 
-    if(rowindex + 1 > len(lines) - 1 or colindex > len(lines[rowindex + 1]) - 1):
+    if(row_index + 1 > len(lines) - 1 or col_index > len(lines[row_index + 1]) - 1):
         pass
     else:
-        occupied_seats.append(lines[rowindex+1][colindex])
+        neighbouring_seats.append(lines[row_index+1][col_index])
 
-    if(rowindex - 1 < 0 or colindex + 1 > len(lines[rowindex - 1]) - 1):
+    if(row_index - 1 < 0 or col_index + 1 > len(lines[row_index - 1]) - 1):
         pass
     else:
-        occupied_seats.append(lines[rowindex-1][colindex+1])
-
-    if(rowindex - 1 < 0 or colindex - 1 < 0):
+        neighbouring_seats.append(lines[row_index-1][col_index+1])
+    if(row_index - 1 < 0 or col_index - 1 < 0):
         pass
     else:
-        occupied_seats.append(lines[rowindex-1][colindex-1])
-
-    if(rowindex - 1 < 0):
+        neighbouring_seats.append(lines[row_index-1][col_index-1])
+    if(row_index - 1 < 0):
         pass
     else: 
-        occupied_seats.append(lines[rowindex-1][colindex])
+        neighbouring_seats.append(lines[row_index-1][col_index])
 
-    return occupied_seats
+    return neighbouring_seats
 
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
     print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 2489
-
-
