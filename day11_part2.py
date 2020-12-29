7aefc5e59b41dc9330070457d9d36df07d0f8094
Simonolsson98 <Simol@student.chalmers.se>
2020-12-29 22:11:55 +0100

    day11 part2 start

--- day11_part2.py
+++ day11_part2.py
@@ -8,7 +8,91 @@ def main():
         lines.append(list(i[:len(i)-1]))
         i = input.readline()
 
+    #deepcopy needed here, since lines is a list of lists
+    new_lines = copy.deepcopy(lines) #the list used to check if a round changed our starting list
+    
+    while True:
+        for i in range(len(lines)):
+            for j in range(len(lines[i])):
+                seat = lines[i][j]
+                neighbouring_seats = get_occupied_seats(lines, i, j, []) #retrieve the neighbouring seats
+
+                if seat == 'L' and '#' not in neighbouring_seats:
+                    new_lines[i][j] = '#' #seat should be occupied
+                elif seat == '#' and neighbouring_seats.count('#') >= 5:
+                    new_lines[i][j] = 'L' #seat gets freed
+                else: #seat = '.' or both of the above requirements were not met
+                    pass
+
+        if lines == new_lines: #no change from the round that just executed, so we should stop
+            result = 0
+            for k in range(len(new_lines)):
+                result += new_lines[k].count('#') #count occurrences of # in every line
+            return result
+        else: #prepare for next round
+            lines = copy.deepcopy(new_lines)#deepcopy needed here, since lines is a list of lists
+
+
+def get_occupied_seats(lines, row_index, col_index, neighbouring_seats): #function that returns a list of all occupied neighbouring seats
+    #checks for all types of indices, also makes sure that for example the first element only has 3 neighbours, since L # gives ['#', '.', '#'] for L
+    #                                                                                                                 . #   
+    
+    if(col_index - 1 < 0):
+        pass
+    else:
+        i = 1
+        while(lines[row_index][col_index - i] == '.'):
+            i += 1 
+        neighbouring_seats.append(lines[row_index][col_index-i])
+    if col_index + 1 > len(lines[row_index]) - 1:
+        pass
+    else:
+        i = 1
+        while(lines[row_index][col_index + i] == '.'):
+            i += 1 
+        neighbouring_seats.append(lines[row_index][col_index+1])
+    if(col_index - 1 < 0 or row_index + 1 > len(lines) - 1):
+        pass
+    else:
+        i = 1
+        while(lines[row_index + i][col_index - i] == '.'):
+            i += 1 
+        neighbouring_seats.append(lines[row_index + i][col_index - i])
+    if(row_index + 1 > len(lines) - 1 or col_index + 1 > len(lines[row_index + 1]) - 1):
+        pass
+    else:
+        i = 1
+        while(lines[row_index + i][col_index + i] == '.'):
+            i += 1 
+        neighbouring_seats.append(lines[row_index+1][col_index+1])
+
+    if(row_index + 1 > len(lines) - 1 or col_index > len(lines[row_index + 1]) - 1):
+        pass
+    else:
+        i = 1
+        while(lines[row_index + i][col_index] == '.'):
+            i += 1 
+        neighbouring_seats.append(lines[row_index+1][col_index])
+
+    if(row_index - 1 < 0 or col_index + 1 > len(lines[row_index - 1]) - 1):
+        pass
+    else:
+        i = 1
+        while(lines[row_index - i][col_index + i] == '.'):
+            i += 1 
+        neighbouring_seats.append(lines[row_index-1][col_index+1])
+    if(row_index - 1 < 0 or col_index - 1 < 0):
+        pass
+    else:
+        neighbouring_seats.append(lines[row_index-1][col_index-1])
+    if(row_index - 1 < 0):
+        pass
+    else: 
+        neighbouring_seats.append(lines[row_index-1][col_index])
+
+    return neighbouring_seats
+
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
-    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 2180
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 2489
