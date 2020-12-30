4c169fffab12145841b55cda13bedd99e3392c61
Simonolsson98 <Simol@student.chalmers.se>
2020-12-30 13:49:23 +0100

    removed useless statement

--- day2_part2.py
+++ day2_part2.py
@@ -14,7 +14,7 @@ def main():
         first_occurrence  = possible_passwords[int(pos1) - 1]
         second_occurrence = possible_passwords[int(pos2) - 1]
         if first_occurrence == letter and second_occurrence == letter: #both -> not valid
-            validPass += 0
+            pass
         elif first_occurrence == letter or second_occurrence == letter: #exactly one -> valid
             validPass += 1
 
