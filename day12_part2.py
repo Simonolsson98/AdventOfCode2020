9ce8d676dd4c4ef80bf4064a08de7053b1bdc550
Simonolsson98 <Simol@student.chalmers.se>
2021-01-01 19:52:51 +0100

    fix compilation errors, using lists instead of tuple since we manipulate data

--- day12_part2.py
+++ day12_part2.py
@@ -5,30 +5,30 @@ def main():
     i = input.readline()
     xpos = 0
     ypos = 0
-    waypoint = (1, 10) #(N/S, E/W)
+    waypoint = [1, 10] #(N/S, E/W)
     while i: #get input into a list of strings
         operator = i[0]
-        range_or_dir = i[1:]
+        range_or_dir = int(i[1:])
         if operator == "R":
-            for i in range(0, range_or_dir / 90): #turn 3 times for 270 degrees, for example
+            for i in range(0, int(range_or_dir / 90)): #turn 3 times for 270 degrees, for example
                 if waypoint[0] >= 0 and waypoint[1] >= 0:
-                    waypoint = (-waypoint[1], waypoint[0])
+                    waypoint = [-waypoint[1], waypoint[0]]
                 elif waypoint[0] < 0 and waypoint[1] >= 0:
-                    waypoint = (-waypoint[1], -waypoint[0])
+                    waypoint = [-waypoint[1], -waypoint[0]]
                 elif waypoint[0] < 0 and waypoint[1] < 0:
-                    waypoint = (-waypoint[1], waypoint[0])
+                    waypoint = [-waypoint[1], waypoint[0]]
                 else:
-                    waypoint = (-waypoint[1], waypoint[0])
+                    waypoint = [-waypoint[1], waypoint[0]]
         elif operator == "L":
-            for i in range(0, range_or_dir / 90):
+            for i in range(0, int(range_or_dir / 90)):
                 if waypoint[0] >= 0 and waypoint[1] >= 0:
-                    waypoint = (waypoint[1], -waypoint[0])
+                    waypoint = [waypoint[1], -waypoint[0]]
                 elif waypoint[0] < 0 and waypoint[1] >= 0:
-                    waypoint = (-waypoint[1], -waypoint[0])
+                    waypoint = [-waypoint[1], -waypoint[0]]
                 elif waypoint[0] < 0 and waypoint[1] < 0:
-                    waypoint = (-waypoint[1], waypoint[0])
+                    waypoint = [-waypoint[1], waypoint[0]]
                 else:
-                    waypoint = (waypoint[1], waypoint[0])
+                    waypoint = [waypoint[1], waypoint[0]]
         elif operator == "F":
             xchange = waypoint[0] * range_or_dir
             ychange = waypoint[1] * range_or_dir
@@ -45,7 +45,7 @@ def main():
         
         i = input.readline()
     return abs(xpos) + abs(ypos) #manhattan distance
-    
+
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
