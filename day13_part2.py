fa9e5484711345a3e8de07bcb92e3c0cd4649a10
Simonolsson98 <Simol@student.chalmers.se>
2021-01-01 23:12:08 +0100

    not correct yet

--- day13_part2.py
+++ day13_part2.py
@@ -1,3 +1,4 @@
+from os import times
 import time
 
 def main():
@@ -5,19 +6,41 @@ def main():
     _ = input.readline()
     buses = input.readline()
     split_buses = buses.split(",")
-    departures = []
-
-    for i in range(len(split_buses)):
-        try: #will only add the integers in split_buses
-            departures.append(int(split_buses[i]))
-        except ValueError: #simply ignore the error
+    departures = list(split_buses)
+    #print(departures)
+    result = -1
+    timestamp = 1
+    max_departure = 0
+    """ for i in range(len(departures)):
+        print(i)
+        try:
+            print(max_departure)
+            if int(departures[i]) > max_departure:
+                print(int(departures[i]))
+                print(i)
+                max_departure = departures[i]
+        except TypeError:
             pass
+        except ValueError:
+            pass """
+    
+    while timestamp >= 1:
+        #print(timestamp)
+        for j in range(len(departures)):
+            try:
+                if((timestamp + j) % int(departures[j]) != 0):
+                    timestamp += (732 - int(departures[j]))
+                    break
+            except ValueError: #ignore 'x' entries
+                pass
+            #print(f"GOT PAST: {departures[j]}")
+            if(j == len(departures)):
+                return timestamp
+    
 
-    chosen_bus = -1
-
-    return None
+    return result
 
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
-    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 2935
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 
