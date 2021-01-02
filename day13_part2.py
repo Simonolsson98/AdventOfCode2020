1e7ba8344fadb9dbc8e2763bf84858ae17babd14
Simonolsson98 <Simol@student.chalmers.se>
2021-01-02 17:39:58 +0100

    some form of CRT

--- day13_part2.py
+++ day13_part2.py
@@ -7,20 +7,22 @@ def main():
     _ = input.readline()
     buses = input.readline()
     departures = list(buses.split(","))
-    print(departures)
-    result = -1
     real_buses = []
-    
-    for j in range(len(departures)):
-        try:
-            int(departures[j])
-            real_buses.append(int(departures[j])) #adds the buses with real ID's to a separate list
-        except ValueError: #ignore 'x' entries
-            pass
-    real_buses.sort()
-    print(real_buses)
 
-    return None
+    
+    mods = {}
+    for idx, bus_id in enumerate(departures):
+        if bus_id != 'x': #ignore x's
+            mods[int(bus_id)] = -idx % int(bus_id)
+
+    iterator = 0
+    increment = 1
+    for bus in mods.keys():
+        while iterator % bus != mods[bus]:
+            iterator += increment
+        increment *= bus
+
+    return iterator
 
 if __name__ == '__main__':
     start_time = time.time()
