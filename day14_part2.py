ba3bd6fdda66ef00b738d259720ba81c98e96ec6
Simonolsson98 <Simol@student.chalmers.se>
2021-01-02 21:47:34 +0100

    still not entirely correct

--- day14_part2.py
+++ day14_part2.py
@@ -19,16 +19,35 @@ def main():
         while len(value) <= 35:
             value = '0' + value #extend to 35 bits
         result = ""
+
         for j in range(len(value)):
-            if mask[j] != 0: # mask has a value, so use this value
-                pass
-            elif mask[j] == 1: #mask == 'X' so take the value from the init program
-                pass
-            else: #mask[j] == 'X'
-                pass
+            #print(mask[j])
+            if mask[j] == '0': # mask == 0 means overwrite bit to 0
+                result += value[j]
+            elif mask[j] == '1' or mask[j] == 'X': #mask == 'X' or 1, so keep this bit as this value
+                result += mask[j]
+        number_of_x = result.count('X')
+        index_pos_list = []
+        index_pos = 0
+        while True: 
+            try: #find all occurrences of 'X', more specifically its indices
+                index_pos = result.index('X', index_pos)
+                index_pos_list.append(index_pos)
+                index_pos += 1
+            except ValueError as e:
+                break
+        temp_result = result
+        result = result.replace('X', '0')
         
-        result = int("0b" + result, base = 0) #add 0b to be able to convert back to int, from binary
-        changed_vals[index] = result
+        for i in range(2 ** len(index_pos_list)):
+            bin_index = bin(i)
+            bin_index = bin_index[2:]
+
+            for j in range(len(bin_index)):
+                print(bin_index[j])
+                result = str(result[:index_pos_list[j]] + bin_index[j] + result[index_pos_list[j] + 1:])
+            result = int("0b" + result, base = 2) #add 0b to be able to convert back to int, from binary
+            changed_vals[index] = result
 
         i = input.readline()
     
