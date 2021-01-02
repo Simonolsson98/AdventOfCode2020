4329d2fbd0ab362ff958a22a02ea66df5fc70fbf
Simonolsson98 <Simol@student.chalmers.se>
2021-01-02 18:42:27 +0100

    getting started with day14

--- day14.py
+++ day14.py
@@ -1,8 +1,28 @@
 import time
 
 def main():
-    input = open("day13_input.txt")
-   
+    input = open("day14_input.txt")
+    i = input.readline()
+    changed_vals = {}
+    mask = ""
+    while i:
+        if i[0:4] == "mask":
+            mask = mask[7:]
+            i = input.readline()
+            continue
+        index = i.split(" = ")[0][4:-1] #:-1 to ignore ]
+        value = i.split(" = ")[1][:-1] #:-1 to ignore newline
+        print(index)
+        print(value)
+        result = 0
+        for j in range(len(value)):
+            if mask[j] != 'X':
+                result.append(mask[j])
+            else: #mask == 'X'
+                result.append(value[j])
+
+
+        i = input.readline()
 
 if __name__ == '__main__':
     start_time = time.time()
