0e800383a232b2612f88f49a4b8176671fa47f37
Simonolsson98 <Simol@student.chalmers.se>
2021-01-02 18:16:08 +0100

    day14 template

---
+++ day14.py
@@ -0,0 +1,10 @@
+import time
+
+def main():
+    input = open("day13_input.txt")
+   
+
+if __name__ == '__main__':
+    start_time = time.time()
+    returnVal = main() 
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 836024966345345
