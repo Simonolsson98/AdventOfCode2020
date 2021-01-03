d763f1483bdffeb60dc55a633a8ccbde92e2a086
Simonolsson98 <Simol@student.chalmers.se>
2021-01-03 23:27:36 +0100

    day15 solution small optimization

--- day15.py
+++ day15.py
@@ -1,22 +1,13 @@
 import time
 
 def main():
-    input = [0,1,5,10,3,12,19] # no real need for i/o here lol
-    last_spoken = 0
-    spoken_vals = []
+    spoken_vals = [0,1,5,10,3,12,19] # no real need for i/o here lol
+    last_spoken = spoken_vals[::-1][0] #fun way to retrieve last element of a list
 
-    for i in range(2020):
-        try: # get the 
-            last_spoken = input[i]
-            spoken_vals.append(last_spoken)
-            continue
-        except IndexError:
-            pass
-
+    for i in range(len(spoken_vals), 2020):
         if(last_spoken not in spoken_vals[:-1]): # if number hasnt appeared before, add it and say 0
             last_spoken = 0
             spoken_vals.append(last_spoken)
-            continue
         else:
             # get the index of the last occurring number that is last_spoken, 
             # and assign last_spoken to the difference between the turn value and this value
@@ -24,6 +15,7 @@ def main():
             spoken_vals.append(last_spoken)
     
     return last_spoken
+    
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
