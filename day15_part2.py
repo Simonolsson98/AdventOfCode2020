699200c49aae55557b085e3e296a25bca83e8d57
Simonolsson98 <Simol@student.chalmers.se>
2021-01-05 00:10:39 +0100

    combined part1 and part2; part2 works now, 26 seconds exec time..

--- day15_part2.py
+++ day15_part2.py
@@ -1,12 +1,11 @@
 import time
 
-def main():
-    spoken_vals = {0:(0,None), 3:(1, None), 6:(2, None)} # no real need for i/o here lol
-    last_spoken = 6
+def main(part):
+    spoken_vals = {0:(0,None), 1:(1,None), 5:(2,None), 10:(3,None),3:(4,None),12:(5,None),19:(6,None)} # no real need for i/o here lol
+    last_spoken = 19
 
-    for i in range(len(spoken_vals), 10):
+    for i in range(len(spoken_vals), part):
         (a, b) = spoken_vals[last_spoken]
-        
         if b == None:
             spoken_vals[0] = (i, spoken_vals[0][0])
             last_spoken = 0
@@ -18,11 +17,13 @@ def main():
             else:
                 last_spoken = a - b
                 spoken_vals[a - b] = (i, None)
-        print(f"{spoken_vals} {last_spoken}")
-    
+
     return last_spoken
     
 if __name__ == '__main__':
     start_time = time.time()
-    returnVal = main() 
-    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer =
+    returnVal = main(2020) 
+    print(f"part 1: answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 1373
+    start_time = time.time()
+    returnVal = main(30_000_000)
+    print(f"part 2: answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 112458
