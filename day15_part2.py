0ab564c9b6fbd48cda907ef79d9bc9f19c16cc18
Simonolsson98 <Simol@student.chalmers.se>
2021-01-04 23:05:22 +0100

    still wrong

--- day15_part2.py
+++ day15_part2.py
@@ -2,10 +2,9 @@ import time
 
 def main():
     spoken_vals = {0:0,1:1,5:2,10:3,3:4,12:5,19:6} # no real need for i/o here lol
-    last_spoken = 19 #fun way to retrieve last element of a list
+    last_spoken = 19
 
     for i in range(len(spoken_vals), 2020):
-        
         index = spoken_vals[last_spoken]
         print(index)
         spoken_vals[last_spoken] = i
