ac9749d2cdc27719318e5ed2d9fea9ce59e47936
Simonolsson98 <Simol@student.chalmers.se>
2021-01-05 02:29:30 +0100

    starting on day16 solution

--- day16.py
+++ day16.py
@@ -2,13 +2,24 @@ import time
 
 def main():
     input = open("day16_input.txt")
-    lines = []
+    lines = ""
     i = input.readline()
     
     while i: #get input into a list of strings
-        lines.append(int(i))
-        i = input.readline()
+        lines += i
+        i = input.readline()    
 
+    split_lines = lines.split("\n\n")
+    all_ranges = split_lines[0]
+    all_ranges = all_ranges.split("\n")
+    ranges = []
+    for line in all_ranges: #get all ranges into a list
+        ranges.append(line.split(": ")[1].split(" or ")[0])
+        ranges.append(line.split(": ")[1].split(" or ")[1])
+
+    #print(ranges)
+    my_ticket = split_lines[1]
+    nearby_tickets = split_lines[2]
     
 if __name__ == '__main__':
     start_time = time.time()
