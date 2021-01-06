c742f7c6db912a625e2e9d5a39deaae0de83a950
Simonolsson98 <Simol@student.chalmers.se>
2021-01-07 00:20:08 +0100

    day16 solution done

--- day16.py
+++ day16.py
@@ -38,4 +38,4 @@ def main():
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
-    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 1373
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 29851
