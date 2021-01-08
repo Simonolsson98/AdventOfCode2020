5fceedbd1341ca4e5e3de77c73db0045b0e5eae5
Simonolsson98 <Simol@student.chalmers.se>
2021-01-08 16:08:42 +0100

    day17 template

--- day17.py
+++ day17.py
@@ -14,4 +14,4 @@ def main():
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
-    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 3029180675981
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer 
