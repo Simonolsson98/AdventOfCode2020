be88d98df13bf10d1eb280f2d5a156daa51fff08
Simonolsson98 <Simol@student.chalmers.se>
2021-01-08 16:08:33 +0100

    day17 template

---
+++ day17.py
@@ -0,0 +1,17 @@
+from os import remove
+import time
+
+def main():
+    input = open("day17_input.txt")
+    lines = ""
+    i = input.readline()
+    
+    while i: # get input into a list of strings
+        lines += i
+        i = input.readline()    
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    returnVal = main() 
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 3029180675981
