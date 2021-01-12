a38b10ee66a660cf541aa5fd4fb40cf176888aab
Simonolsson98 <Simol@student.chalmers.se>
2021-01-12 19:55:05 +0100

    wrong answer, something wrong with paranthesis I think

--- day18.py
+++ day18.py
@@ -4,48 +4,22 @@ def main():
     input = open("day18_input.txt")
     i = input.readline()    
     result = 0
+    substr = ""
     while i: # get input into a list of lists
-        subresult = 0
         for j in range(len(i)):
-            index = j
-            substr = ""
-            left_par = 1
-            if i[index] == "(":
-                while i[index] != ")" or left_par == 1:
-                    if i[index] == "(":
-                        left_par += 1
-                    substr += i[index]
-                    index += 1
-                    left_par -= 1
-                substr += i[index] #add closing parenthesis too
-                opening_inner_par = 0
-                closing_inner_par = 0
-                iter = 0
-                while substr.count("(") > 0:
-                    print(f"substr: {substr} for iter: {iter}")
-                    if substr[iter] == "(":
-                        opening_inner_par = iter
-                    if substr[iter] == ")":
-                        closing_inner_par = iter
-                        print(substr)
-                        print(f"open: {opening_inner_par}, closing: {closing_inner_par}")
-                        print(str(eval(substr[opening_inner_par:closing_inner_par+1])))
-                        substr = substr[:opening_inner_par] + str(eval(substr[opening_inner_par:closing_inner_par+1])) + substr[closing_inner_par+1:]
-                        print(substr)
-                        iter = 0
-                        continue
-                    
-                    iter += 1
-                
+            substr = substr + i[j]
+            #print(substr)
+            try:
+                substr = str(eval(substr))
                 #print(eval(substr))
-                print(i)
-                i = i[:j] + str(eval(substr[1:])) + i[index+1:]
-                print(i)
-                return
-
-        
+            except SyntaxError:
+                pass
+        result += int(substr)
+        #print(int(substr))
+        substr = ""
         i = input.readline()
-    
+
+    return result
 
 
 
