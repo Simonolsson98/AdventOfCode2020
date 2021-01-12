27ddb4c7567d650ae60b47aaf80fcbea672bea7f
Simonolsson98 <Simol@student.chalmers.se>
2021-01-12 21:47:54 +0100

    day18 solution

--- day18.py
+++ day18.py
@@ -9,20 +9,38 @@ def main():
     while i: # get input into a list of lists
         for j in range(len(i)):
             substr = substr + i[j]
-            print(substr)
+
             pares = ""
             while substr[0] == "(" and len(substr) != substr.count("("):
                 pares += substr[0]
                 substr = substr[1:]
-            try:
-                substr = str(eval(substr))
+            if substr.count(")") >= 1: #if unmatching parentheses'
                 substr = pares + substr
-                print(substr)
-            except SyntaxError:
-                substr = pares + substr
+                try:
+                    index = 0
+                    for k in range(len(substr)):
+                        if substr[k] == "(":
+                            index = k
+                    substr = substr[:index] + str(eval(substr[index:]))
+                except SyntaxError:
+                    substr = pares + substr
+            else:
+                try:
+                    substr = str(eval(substr))
+                    substr = pares + substr
+                except SyntaxError:
+                    substr = pares + substr
+                    index = 0
+                    for k in range(len(substr)):
+                        if substr[k] == "(":
+                            index = k
+                    try:                   
+                        substr = substr[:index+1] + str(eval(substr[index+1:]))
+                    except SyntaxError:
+                        pass
 
-        result += int(substr)
-        print(f"row: {iter} for {int(substr)}")
+        result += int(eval(substr)) #add result from the row
+        print(int(eval(substr)))
         substr = ""
         i = input.readline()
         iter += 1
@@ -34,4 +52,4 @@ def main():
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
-    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer 
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 209335026987
