560f78ba1f2619bb5f6e3809ff0d564a399b2998
Simonolsson98 <Simol@student.chalmers.se>
2021-01-12 21:53:39 +0100

    day18 part2 template

---
+++ day18_part2.py
@@ -0,0 +1,17 @@
+import time
+
+def main():
+    input = open("day18_input.txt")
+    i = input.readline()    
+    result = 0
+    substr = ""
+    iter = 1
+    while i: # get input into a list of lists
+        for j in range(len(i)):
+            substr = substr + i[j]
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    returnVal = main() 
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 
