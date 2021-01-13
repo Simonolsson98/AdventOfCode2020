5899b1ca2f555d1d4024d6d2ba93e1787df2accf
Simonolsson98 <Simol@student.chalmers.se>
2021-01-13 21:53:29 +0100

    testing a theory

--- day18_part2.py
+++ day18_part2.py
@@ -9,6 +9,24 @@ def main():
     while i: # get input into a list of lists
         for j in range(len(i)):
             substr = substr + i[j]
+    pares = substr.count("(")
+    start_index = 0
+    end_index = 0
+    for i in range(len(pares)): #trying to evaluate each parentheses individually
+        for j in range(len(substr)):
+            if substr[j] == "(":
+                start_index = j
+            elif substr[j] == ")":
+                end_index = j
+                break
+        for k in range(start_index, end_index):
+            try: 
+                eval(substr[:k])
+            except SyntaxError:
+                pass
+
+
+    
 
 
 if __name__ == '__main__':
