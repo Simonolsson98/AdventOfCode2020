1bada472119bacd00b9f6a771e01fdfa22da6f84
Simonolsson98 <Simol@student.chalmers.se>
2021-01-13 22:46:08 +0100

    fixed, forgot to reset string after each iteration of input

--- day18_part2.py
+++ day18_part2.py
@@ -9,11 +9,12 @@ def main():
     while i: # get input into a list of lists
         for j in range(len(i)):
             substr = substr + i[j]
-        if substr[-1] == '\n':
+        if substr[-1] == '\n': #remove the newline
             substr = substr[:-1]
         pares = substr.count("(")
         start_index = 0
         end_index = 0
+
         for i in range(pares): #trying to evaluate each parentheses individually
             for j in range(len(substr)):
                 if substr[j] == "(":
@@ -28,29 +29,27 @@ def main():
                     sub_eval += str(eval(expr))
                 except SyntaxError:
                     pass
-                sub_eval += " * "
+                sub_eval += " * " #add the operator back again, after evaluating the (addition) operation
             sub_eval = sub_eval[:-3] #remove the last multiplication operator, ugly but works..
             substr = substr[:start_index] + str(eval(sub_eval)) + substr[end_index+1:]
-        #we reach a state where we dont have any parentheses left (hopefully), so just split on * again and eval
-        #print(f"PLS: {substr}")
 
+        #we reach a state where we dont have any parentheses left, so just split on * again and eval
         split_on_plus = substr.split(" * ")
-        sub_eval = 1
+        last_expr = 1
         for expr in split_on_plus:
             try:
-                sub_eval *= eval(expr)
+                last_expr *= eval(expr)
             except SyntaxError:
                 pass
-        result += sub_eval
+
+        result += last_expr
         split_on_plus = []
+        substr = ""
         i = input.readline()
 
     return result
 
-    
-
-
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
-    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 33331817392479
