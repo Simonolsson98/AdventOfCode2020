5c4b280f334b98e02054ecfad8b265979bc59bfe
Simonolsson98 <Simol@student.chalmers.se>
2021-01-13 22:39:12 +0100

    wrong answer still, but looks like its doing the parsing correctly?

--- day18_part2.py
+++ day18_part2.py
@@ -9,7 +9,8 @@ def main():
     while i: # get input into a list of lists
         for j in range(len(i)):
             substr = substr + i[j]
-        #print(substr)
+        if substr[-1] == '\n':
+            substr = substr[:-1]
         pares = substr.count("(")
         start_index = 0
         end_index = 0
@@ -20,27 +21,20 @@ def main():
                 elif substr[j] == ")":
                     end_index = j
                     break
-                split_on_plus = substr[start_index+1:end_index].split(" * ")
-                sub_eval = ""
-                for expr in split_on_plus:
-                    try:
-                        sub_eval += str(eval(expr))
-                    except SyntaxError:
-                        pass
-                    sub_eval += " * "
-                sub_eval = sub_eval[:-3] #remove the last multiplication operator, ugly but works..
-                substr = substr[:start_index] + str(eval(sub_eval)) + substr[end_index+1:]
-
-            split_on_plus = substr.split(" * ")
-            for k in range(start_index, end_index):  
-                try: 
-                    eval(substr[:k])
+            split_on_plus = substr[start_index+1:end_index].split(" * ")
+            sub_eval = ""
+            for expr in split_on_plus:
+                try:
+                    sub_eval += str(eval(expr))
                 except SyntaxError:
                     pass
-            #we reach a state where we dont have any parentheses left (hopefully), so just split on * again and eval
+                sub_eval += " * "
+            sub_eval = sub_eval[:-3] #remove the last multiplication operator, ugly but works..
+            substr = substr[:start_index] + str(eval(sub_eval)) + substr[end_index+1:]
+        #we reach a state where we dont have any parentheses left (hopefully), so just split on * again and eval
+        #print(f"PLS: {substr}")
+
         split_on_plus = substr.split(" * ")
-            #print(substr)
-        print(split_on_plus)
         sub_eval = 1
         for expr in split_on_plus:
             try:
