d1f0e00ae6cebc2e5889e317d9042e857776305e
Simonolsson98 <Simol@student.chalmers.se>
2021-01-14 00:08:37 +0100

    day19 template

--- day19.py
+++ day19.py
@@ -2,13 +2,10 @@ import time
 
 def main():
     input = open("day19_input.txt")
-    i = input.readline()    
-    result = 0
-    substr = ""
-    iter = 1
-    while i: # get input into a list of lists
-        for j in range(len(i)):
-            substr = substr + i[j]
+    i = input.readline()   
+    while i:
+
+        i = input.readline()
 
 
 if __name__ == '__main__':
