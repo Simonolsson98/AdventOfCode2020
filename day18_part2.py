2741a6e56c486e19b2ff589dd1506c6697664a01
Simonolsson98 <Simol@student.chalmers.se>
2021-01-13 22:32:18 +0100

    some problems with nested if loops still

--- day18_part2.py
+++ day18_part2.py
@@ -5,10 +5,11 @@ def main():
     i = input.readline()    
     result = 0
     substr = ""
-    iter = 1
+
     while i: # get input into a list of lists
         for j in range(len(i)):
             substr = substr + i[j]
+        #print(substr)
         pares = substr.count("(")
         start_index = 0
         end_index = 0
@@ -19,29 +20,38 @@ def main():
                 elif substr[j] == ")":
                     end_index = j
                     break
-            split_on_plus = substr[start_index+1:end_index].split(" * ")
-            print(substr)
-            print(split_on_plus)
-            result = ""
-            for expr in split_on_plus:
-                try:
-                    result += str(eval(expr))
-                except SyntaxError:
-                    pass
-                result += " * "
-            result = result[:-3] #remove the last multiplication operator, ugly but works..
-            print(result)
-            substr = substr[:start_index] + str(eval(result)) + substr[end_index+1:]
-            print(substr)
-            for k in range(start_index, end_index): #we reach a state where we dont have any parentheses left (hopefully)
-                
+                split_on_plus = substr[start_index+1:end_index].split(" * ")
+                sub_eval = ""
+                for expr in split_on_plus:
+                    try:
+                        sub_eval += str(eval(expr))
+                    except SyntaxError:
+                        pass
+                    sub_eval += " * "
+                sub_eval = sub_eval[:-3] #remove the last multiplication operator, ugly but works..
+                substr = substr[:start_index] + str(eval(sub_eval)) + substr[end_index+1:]
+
+            split_on_plus = substr.split(" * ")
+            for k in range(start_index, end_index):  
                 try: 
                     eval(substr[:k])
                 except SyntaxError:
                     pass
-        print(eval(substr))
+            #we reach a state where we dont have any parentheses left (hopefully), so just split on * again and eval
+        split_on_plus = substr.split(" * ")
+            #print(substr)
+        print(split_on_plus)
+        sub_eval = 1
+        for expr in split_on_plus:
+            try:
+                sub_eval *= eval(expr)
+            except SyntaxError:
+                pass
+        result += sub_eval
+        split_on_plus = []
         i = input.readline()
 
+    return result
 
     
 
