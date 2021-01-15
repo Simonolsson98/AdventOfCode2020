44ef56e390a8496ef0dbe608a7aedcdaf34d587b
Simonolsson98 <Simol@student.chalmers.se>
2021-01-15 22:39:52 +0100

    trying to figure out how to check rules

--- day19.py
+++ day19.py
@@ -14,16 +14,19 @@ def main():
     rule_dict = {}
     char_rule = []
     for rule in rules:
-        if(re.match(r"letter", rule[4:])):
+        if(re.match(r"[a-z]", rule[4:])):
             char_rule.append(rule)
         try:
             either_rule = rule.split(" | ")
             either_rule[0] = either_rule[0].split(":")[1][1:]
-            rule_dict[rule[0:1]] = either_rule
+            rule_dict[rule[0:2]] = either_rule
             #sub_rules = either_rule.split(" ")
         except IndentationError:
             pass
-        
+    valid_messages = 0
+    for message in messages:
+        for rule in rule_dict.keys():
+            pass
     print(rule_dict)
     return None
 if __name__ == '__main__':
