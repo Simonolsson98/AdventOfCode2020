e2d15ceab10062e6fc3d1baf6d795e6d12b87c9d
Simonolsson98 <Simol@student.chalmers.se>
2021-01-23 22:40:06 +0100

    I/O

--- day20.py
+++ day20.py
@@ -3,12 +3,11 @@ import time
 def main():
     input = open("day20_input.txt")
     i = input.readline()    
-    result = 0
     substr = ""
-    iter = 1
     while i: # get input into a list of lists
-        for j in range(len(i)):
-            substr = substr + i[j]
+        substr = substr + i
+    grids = substr.split("\n")
+    
 
 
 
