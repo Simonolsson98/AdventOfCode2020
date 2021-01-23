04787aa8380116bacba7aacb4a8f4aa7aa51d536
Simonolsson98 <Simol@student.chalmers.se>
2021-01-23 23:52:40 +0100

    started adding all edges to lists

--- day20.py
+++ day20.py
@@ -5,17 +5,31 @@ def main():
     i = input.readline()    
     substr = ""
     while i: # get input into a list of lists
+        #i = i[:-1]
         substr = substr + i
         i = input.readline()
     grids = substr.split("\n\n")
     tiles = []
-    for grid in grids:
-        tiles.append(grid.split(":")[0].split(" ")[1])
-        grid = grid.split(":")[1]
+    top_row = []
+    bottom_row = []
+    right_col = []
+    left_col = []
+    indiv_grids = []
+    for i in range(len(grids)):
+        tiles.append(grids[i].split(":")[0].split(" ")[1])
+        grids[i] = grids[i].split(":")[1]
+        indiv_grids.append(grids[i].split("\n")[1:])
 
-
-    print(grids)
-    print(tiles)
+    for each_grid in indiv_grids: #adding each edge to lists for further checks
+        top_row.append(each_grid[0])
+        bottom_row.append(each_grid[-1])
+        tempR = ""
+        tempL = ""
+        for row in each_grid:
+            tempR = tempR + row[-1]
+            tempL = tempL + row[0]
+        right_col.append(tempR)
+        left_col.append(tempL)
 
 
 
